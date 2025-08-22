6e9f9dd9533fd68c4fa86d277fdd0f8af947f0de
Maria Clara <s.pnhbabagonio@usm.edu.ph>
2025-08-22 21:31:26 +0800

    add usermanagement and fixed dashboard ui

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -2,10 +2,21 @@
 import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { 
+    Sidebar, 
+    SidebarContent, 
+    SidebarFooter, 
+    SidebarHeader, 
+    SidebarMenu, 
+    SidebarMenuButton, 
+    SidebarMenuItem,
+    SidebarMenuSub,
+    SidebarMenuSubItem
+} from '@/components/ui/sidebar';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, Folder, LayoutGrid, CreditCard, Calendar, BarChart3, HelpCircle, Globe, User, Users, UserPlus, Shield, ChevronRight } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -14,17 +25,56 @@ const mainNavItems: NavItem[] = [
         href: '/dashboard',
         icon: LayoutGrid,
     },
+    {
+        title: 'Payments',
+        href: '/payments',
+        icon: CreditCard,
+    },
+    {
+        title: 'Events',
+        href: '/events',
+        icon: Calendar,
+    },
+    {
+        title: 'Reports',
+        href: '/reports',
+        icon: BarChart3,
+    },
+    {
+        title: 'Help & Support',
+        href: '/support',
+        icon: HelpCircle,
+    },
+];
+
+// User Management sub-items
+const userManagementItems = [
+    {
+        title: 'All Users',
+        href: '/users',
+        icon: Users,
+    },
+    {
+        title: 'Add User',
+        href: '/users/create',
+        icon: UserPlus,
+    },
+    {
+        title: 'User Roles',
+        href: '/users/roles',
+        icon: Shield,
+    },
 ];
 
 const footerNavItems: NavItem[] = [
     {
-        title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: Folder,
+        title: 'PSITS Portal',
+        href: 'https://psits.org',
+        icon: Globe,
     },
     {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
+        title: 'Platform Guide',
+        href: '/guide',
         icon: BookOpen,
     },
 ];
@@ -43,11 +93,54 @@ const footerNavItems: NavItem[] = [
                 </SidebarMenuItem>
             </SidebarMenu>
         </SidebarHeader>
-
         <SidebarContent>
-            <NavMain :items="mainNavItems" />
-        </SidebarContent>
+            <SidebarMenu>
+                <!-- Dashboard item -->
+                <SidebarMenuItem>
+                    <SidebarMenuButton as-child>
+                        <Link :href="mainNavItems[0].href">
+                            <component :is="mainNavItems[0].icon" />
+                            <span>{{ mainNavItems[0].title }}</span>
+                        </Link>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+                
+                <!-- Collapsible User Management -->
+                <Collapsible class="group/collapsible">
+                    <SidebarMenuItem>
+                        <CollapsibleTrigger as-child>
+                            <SidebarMenuButton>
+                                <User />
+                                <span>User Management</span>
+                                <ChevronRight class="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                            </SidebarMenuButton>
+                        </CollapsibleTrigger>
+                        <CollapsibleContent>
+                            <SidebarMenuSub>
+                                <SidebarMenuSubItem v-for="subItem in userManagementItems" :key="subItem.title">
+                                    <SidebarMenuButton as-child size="sm">
+                                        <Link :href="subItem.href">
+                                            <component :is="subItem.icon" />
+                                            <span>{{ subItem.title }}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuSubItem>
+                            </SidebarMenuSub>
+                        </CollapsibleContent>
+                    </SidebarMenuItem>
+                </Collapsible>
 
+                <!-- Remaining nav items -->
+                <SidebarMenuItem v-for="item in mainNavItems.slice(1)" :key="item.title">
+                    <SidebarMenuButton as-child>
+                        <Link :href="item.href">
+                            <component :is="item.icon" />
+                            <span>{{ item.title }}</span>
+                        </Link>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarContent>
         <SidebarFooter>
             <NavFooter :items="footerNavItems" />
             <NavUser />
