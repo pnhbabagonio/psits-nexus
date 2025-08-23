e82f528507a8f4edb7fffd4b3a23ca1710c0ff75
Baililian <salibaililian.grade10@gmail.coom>
2025-08-23 13:33:53 +0800

    User Management's Commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -18,9 +17,15 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+    'name',
+    'email',
+    'password',
+    'student_id', 
+    'program',
+    'year',
+    'role',
+    'status',
+    'last_login', 
     ];
 
     /**
